fdaf23236e568173ba0b5fe69fb799a46ac21c68
Izumi1997 <kohey-bsk.0720@outlook.com>
2024-05-19 23:48:03 +0900

    画像検索用のfetchImages関数を追加し、非同期処理用のasyncキーワードとawaitを追記した。

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,6 +21,14 @@ class PixabayPage extends StatefulWidget {
 
   @override
   State<PixabayPage> createState() => _PixabayPageState();
+}
+
+//非同期の関数のため、返り値の方にFutureがつき、asyncキーワードが追加された
+Future<void> fetchImages() async {
+  //awaitで待つことで、Respoce型のデータを受け取っている
+  Response response = await Dio().get(
+      'https://pixabay.com/api/?key=43900472-a82cdd993bf6549e351194a4b&q=yellow+flowers&image_type=photo');
+  print(response.data);
 }
 
 class _PixabayPageState extends State<PixabayPage> {
