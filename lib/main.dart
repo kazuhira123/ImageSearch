d39da9c4019386e16826be00d7e7201bd001302d
Izumi1997 <kohey-bsk.0720@outlook.com>
2024-05-20 00:39:42 +0900

    取得した画像を格納するimageListリストを新規作成 上記の変数を画面に反映するためのWidgetを新規作成

--- lib/main.dart
+++ lib/main.dart
@@ -24,12 +24,17 @@ class PixabayPage extends StatefulWidget {
 }
 
 class _PixabayPageState extends State<PixabayPage> {
+  List imageList = [];
   //非同期の関数のため、返り値の方にFutureがつき、asyncキーワードが追加された
   Future<void> fetchImages() async {
     //awaitで待つことで、Respoce型のデータを受け取っている
     Response response = await Dio().get(
         'https://pixabay.com/api/?key=43900472-a82cdd993bf6549e351194a4b&q=yellow+flowers&image_type=photo');
     print(response.data);
+
+    //定義したリストに取得したデータを代入し、setState関数で画面を更新する
+    imageList = response.data['hits'];
+    setState(() {});
   }
 
   //初回に一度だけ実行されるinitState関数を作成
@@ -41,6 +46,21 @@ class _PixabayPageState extends State<PixabayPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            //画像を横に並べる際の個数を3個に指定
+            crossAxisCount: 3),
+        //Listの要素数をitemCountプロパティlengthで取得する(今回は20)
+        itemCount: imageList.length,
+        //indexにはListの要素が0から順番に入る
+        itemBuilder: (context, index) {
+          //image変数にindex番号に紐づいたListの各要素が順番に代入される
+          Map<String, dynamic> image = imageList[index];
+          //プレビュー用の画像データがあるURLはpreviewURLのvalueに入っている
+          return Image.network(image['previewURL']);
+        },
+      ),
+    );
   }
 }
