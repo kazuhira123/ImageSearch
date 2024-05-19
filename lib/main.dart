da37dbb1224e53e311a98e747e4c91d92245271c
Izumi1997 <kohey-bsk.0720@outlook.com>
2024-05-20 00:04:16 +0900

    fetchImagesで取得した値を表示するためのinitState関数を追記

--- lib/main.dart
+++ lib/main.dart
@@ -23,15 +23,22 @@ class PixabayPage extends StatefulWidget {
   State<PixabayPage> createState() => _PixabayPageState();
 }
 
-//非同期の関数のため、返り値の方にFutureがつき、asyncキーワードが追加された
-Future<void> fetchImages() async {
-  //awaitで待つことで、Respoce型のデータを受け取っている
-  Response response = await Dio().get(
-      'https://pixabay.com/api/?key=43900472-a82cdd993bf6549e351194a4b&q=yellow+flowers&image_type=photo');
-  print(response.data);
-}
-
 class _PixabayPageState extends State<PixabayPage> {
+  //非同期の関数のため、返り値の方にFutureがつき、asyncキーワードが追加された
+  Future<void> fetchImages() async {
+    //awaitで待つことで、Respoce型のデータを受け取っている
+    Response response = await Dio().get(
+        'https://pixabay.com/api/?key=43900472-a82cdd993bf6549e351194a4b&q=yellow+flowers&image_type=photo');
+    print(response.data);
+  }
+
+  //初回に一度だけ実行されるinitState関数を作成
+  @override
+  void initState() {
+    super.initState();
+    fetchImages();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
